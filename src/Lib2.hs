ccb3f1b36b549e392d69c84cf8d08cb441abe415
Dziugas <dziugas.budreika@gmail.com>
2023-10-25 13:53:12 +0300

    added tests for parseStatement function

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -3,18 +3,19 @@
 module Lib2
   ( parseStatement,
     ParsedStatement (..),
-    executeStatement
+    executeStatement,
+    ColumnName (..),
+    Condition (..),
   )
 where
 
-import DataFrame (Column (..), DataFrame(..))
-import InMemoryTables (TableName, database)
-import Data.Char (toLower, isAlphaNum)
-import Lib1 (findTableByName)
+import Data.Char (isAlphaNum, toLower)
 import Data.Maybe (isJust)
-
+import DataFrame (Column (..), DataFrame (..))
+import InMemoryTables (TableName, database)
 
 type ErrorMessage = String
+
 type Database = [(TableName, DataFrame)]
 
 data ColumnName = ColumnName String (Maybe Condition)
@@ -54,43 +55,39 @@ parseShowStatement (secondWord : remaining) = case map toLower secondWord of
     _ -> Left "Invalid show statement: there can only be one table"
   _ -> Left "Invalid show statement: after SHOW keyword no keyword TABLE or TABLES was found"
 
-
 parseSelectStatement :: [String] -> Either ErrorMessage ParsedStatement
 parseSelectStatement input =
   let (columns, rest) = span (not . isFrom) input
-      columnsUnworded = unwords(columns)
+      columnsUnworded = unwords (columns)
       fromKeyword = map toLower (if null rest then "" else rest !! 0)
       whereKeyword = map toLower (rest !! 2)
-      conditions = drop 3 rest  
+      conditions = drop 3 rest
       tableName = rest !! 1
-  in
-  if (fromKeyword == "from")
-    then do    
-      existingColumns <- getAllColumns tableName
-      parsedColumns <- (if (checkForAsterix (words columnsUnworded)) then return [ColumnName "*" Nothing] else parseColumns columnsUnworded)
-      initializer <- checkColumnConditions parsedColumns 
-      if length rest == 2
-        then
-          Right (Select parsedColumns tableName [])
-      else if whereKeyword == "where" && (length rest) >= 6 
+   in if (fromKeyword == "from")
         then do
-          parsedConditions <- parseConditions conditions existingColumns
-          Right (Select parsedColumns tableName parsedConditions)
-      else
-        Left "Invalid select statement: the keyword WHERE is not writen in the appropriate position or the condition in the WHERE clause is not valid" 
-  else
-    Left "Invalid select statement: the keyword FROM is not writen in the appropriate position"
+          existingColumns <- getAllColumns tableName
+          parsedColumns <- (if (checkForAsterix (words columnsUnworded)) then return [ColumnName "*" Nothing] else parseColumns columnsUnworded)
+          initializer <- checkColumnConditions parsedColumns
+          if length rest == 2
+            then Right (Select parsedColumns tableName [])
+            else
+              if whereKeyword == "where" && (length rest) >= 6
+                then do
+                  parsedConditions <- parseConditions conditions existingColumns
+                  Right (Select parsedColumns tableName parsedConditions)
+                else Left "Invalid select statement: the keyword WHERE is not writen in the appropriate position or the condition in the WHERE clause is not valid"
+        else Left "Invalid select statement: the keyword FROM is not writen in the appropriate position"
 
 isFrom :: String -> Bool
 isFrom word = map toLower word == "from"
 
 getAllColumns :: String -> Either ErrorMessage [ColumnName]
 getAllColumns tableName =
-   case lookup tableName database of
+  case lookup tableName database of
     Nothing -> Left "no table found by that name"
-    Just table ->  Right $ (\(DataFrame columns _) -> map (\(Column name _) -> ColumnName name Nothing) columns) table
+    Just table -> Right $ (\(DataFrame columns _) -> map (\(Column name _) -> ColumnName name Nothing) columns) table
 
-checkForAsterix :: [String] -> Bool 
+checkForAsterix :: [String] -> Bool
 checkForAsterix ["*"] = True
 checkForAsterix _ = False
 
@@ -104,13 +101,6 @@ checkColumnConditions colNames =
 hasCondition :: ColumnName -> Bool
 hasCondition (ColumnName _ condition) = isJust condition
 
-
-
-
-
-
-
-
 parseColumns :: String -> Either ErrorMessage [ColumnName]
 parseColumns input = do
   let colNames = splitColNames input
@@ -118,24 +108,24 @@ parseColumns input = do
   return colNameStructures
 
 splitColNames :: String -> [String]
-splitColNames input = customSplit ',' input 
+splitColNames input = customSplit ',' input
 
 customSplit :: Char -> String -> [String]
 customSplit _ [] = [""]
-customSplit delimiter (x:xs)
-    | x == delimiter = "" : rest
-    | otherwise = (x : head rest) : tail rest
+customSplit delimiter (x : xs)
+  | x == delimiter = "" : rest
+  | otherwise = (x : head rest) : tail rest
   where
     rest = customSplit delimiter xs
 
 constructColumnName :: [String] -> Either ErrorMessage ColumnName
 constructColumnName [col]
-    | (containsOnlyLettersAndNumbers col) = Right (ColumnName col Nothing)
-    | otherwise = Left "the column name contains symbols that are not allowed"
+  | (containsOnlyLettersAndNumbers col) = Right (ColumnName col Nothing)
+  | otherwise = Left "the column name contains symbols that are not allowed"
 constructColumnName [agg, col]
-    | lowerAgg == "min" && containsOnlyLettersAndNumbers col = Right (ColumnName col (Just Min))
-    | lowerAgg == "avg" && containsOnlyLettersAndNumbers col = Right (ColumnName col (Just Avg))
-    | otherwise = Left "the column name or aggregate function contains symbols that are not allowed"
+  | lowerAgg == "min" && containsOnlyLettersAndNumbers col = Right (ColumnName col (Just Min))
+  | lowerAgg == "avg" && containsOnlyLettersAndNumbers col = Right (ColumnName col (Just Avg))
+  | otherwise = Left "the column name or aggregate function contains symbols that are not allowed"
   where
     lowerAgg = map toLower agg
 constructColumnName _ = Left "Invalid column name structure"
@@ -143,50 +133,47 @@ constructColumnName _ = Left "Invalid column name structure"
 containsOnlyLettersAndNumbers :: String -> Bool
 containsOnlyLettersAndNumbers = all isAlphaNum
 
-
-
-
-
-
-
-
-
 parseConditions :: [String] -> [ColumnName] -> Either ErrorMessage [Condition]
 parseConditions [] columns = Right []
-parseConditions input columns = 
-    case break (\x -> (map toLower x) == "and") input of
-      (conditionTokens, andKeyword : rest) -> do
-        condition <- parseToken conditionTokens columns
-        conditions <- parseConditions rest columns
-        return (condition : conditions)
-      _ -> do
-        condition <- parseToken input columns
-        return [condition]
+parseConditions input columns =
+  case break (\x -> (map toLower x) == "and") input of
+    (conditionTokens, andKeyword : rest) -> do
+      condition <- parseToken conditionTokens columns
+      conditions <- parseConditions rest columns
+      return (condition : conditions)
+    _ -> do
+      condition <- parseToken input columns
+      return [condition]
 
 parseToken :: [String] -> [ColumnName] -> Either ErrorMessage Condition
 parseToken (colName : op : value : []) parsedColumns
-    | not (matchesAnyInList colName parsedColumns) && not (matchesAnyInList value parsedColumns) = Left "Invalid column name inside a condition"
-    | (matchesAnyInList colName parsedColumns) && (matchesAnyInList value parsedColumns) = Left "Cant do operations with two columns"
-    | otherwise = 
-        case findColumnsPosition colName value parsedColumns of
-              (ColumnName name _, validVal) ->
-                  case op of
-                      "=" -> if colName == name
-                             then Right (Equals (ColumnName name Nothing) validVal)
-                             else Right (Equals (ColumnName name Nothing) validVal)
-                      "<" -> if colName == name
-                             then Right (LessThan (ColumnName name Nothing) validVal)
-                             else Right (GreaterThan (ColumnName name Nothing) validVal)
-                      ">" -> if colName == name
-                             then Right (GreaterThan (ColumnName name Nothing) validVal)
-                             else Right (LessThan (ColumnName name Nothing) validVal)
-                      ">=" -> if colName == name
-                              then Right (GreaterEqualThan (ColumnName name Nothing) validVal)
-                              else Right (LessEqualThan (ColumnName name Nothing) validVal)
-                      "<=" -> if colName == name
-                              then Right (LessEqualThan (ColumnName name Nothing) validVal)
-                              else Right (GreaterEqualThan (ColumnName name Nothing) validVal)
-                      _ -> Left "Invalid operator inside a condition"
+  | not (matchesAnyInList colName parsedColumns) && not (matchesAnyInList value parsedColumns) = Left "Invalid column name inside a condition"
+  | (matchesAnyInList colName parsedColumns) && (matchesAnyInList value parsedColumns) = Left "Cant do operations with two columns"
+  | otherwise =
+      case findColumnsPosition colName value parsedColumns of
+        (ColumnName name _, validVal) ->
+          case op of
+            "=" ->
+              if colName == name
+                then Right (Equals (ColumnName name Nothing) validVal)
+                else Right (Equals (ColumnName name Nothing) validVal)
+            "<" ->
+              if colName == name
+                then Right (LessThan (ColumnName name Nothing) validVal)
+                else Right (GreaterThan (ColumnName name Nothing) validVal)
+            ">" ->
+              if colName == name
+                then Right (GreaterThan (ColumnName name Nothing) validVal)
+                else Right (LessThan (ColumnName name Nothing) validVal)
+            ">=" ->
+              if colName == name
+                then Right (GreaterEqualThan (ColumnName name Nothing) validVal)
+                else Right (LessEqualThan (ColumnName name Nothing) validVal)
+            "<=" ->
+              if colName == name
+                then Right (LessEqualThan (ColumnName name Nothing) validVal)
+                else Right (GreaterEqualThan (ColumnName name Nothing) validVal)
+            _ -> Left "Invalid operator inside a condition"
 parseToken _ _ = Left "Invalid condition"
 
 matchesAnyInList :: String -> [ColumnName] -> Bool
@@ -194,14 +181,8 @@ matchesAnyInList input = any (\(ColumnName name _) -> name == input)
 
 findColumnsPosition :: String -> String -> [ColumnName] -> (ColumnName, String)
 findColumnsPosition colName value parsedColumns
-    | matchesAnyInList colName parsedColumns && not (matchesAnyInList value parsedColumns) = (ColumnName colName Nothing, value)
-    | not (matchesAnyInList colName parsedColumns) && matchesAnyInList value parsedColumns = (ColumnName value Nothing, colName)
-
-
-
-
-
-
+  | matchesAnyInList colName parsedColumns && not (matchesAnyInList value parsedColumns) = (ColumnName colName Nothing, value)
+  | not (matchesAnyInList colName parsedColumns) && matchesAnyInList value parsedColumns = (ColumnName value Nothing, colName)
 
 -- Executes a parsed statemet. Produces a DataFrame. Uses
 -- InMemoryTables.databases a source of data.
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,35 +2,38 @@ import Data.Either
 import Data.Maybe ()
 import InMemoryTables qualified as D
 import Lib1
+import Lib2 (ColumnName (..), Condition (..), ParsedStatement (Select, ShowTable, ShowTables))
+import Lib2 qualified
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "Lib1.findTableByName" $ do
-    it "handles empty lists" $ do
-      Lib1.findTableByName [] "" `shouldBe` Nothing
-    it "handles empty names" $ do
-      Lib1.findTableByName D.database "" `shouldBe` Nothing
-    it "can find by name" $ do
-      Lib1.findTableByName D.database "employees" `shouldBe` Just (snd D.tableEmployees)
-    it "can find by case-insensitive name" $ do
-      Lib1.findTableByName D.database "employEEs" `shouldBe` Just (snd D.tableEmployees)
-  describe "Lib1.parseSelectAllStatement" $ do
-    it "handles empty input" $ do
-      Lib1.parseSelectAllStatement "" `shouldSatisfy` isLeft
-    it "handles invalid queries" $ do
-      Lib1.parseSelectAllStatement "select from dual" `shouldSatisfy` isLeft
-    it "returns table name from correct queries" $ do
-      Lib1.parseSelectAllStatement "selecT * from dual;" `shouldBe` Right "dual"
-  describe "Lib1.validateDataFrame" $ do
-    it "finds types mismatch" $ do
-      Lib1.validateDataFrame (snd D.tableInvalid1) `shouldSatisfy` isLeft
-    it "finds column size mismatch" $ do
-      Lib1.validateDataFrame (snd D.tableInvalid2) `shouldSatisfy` isLeft
-    it "reports different error messages" $ do
-      Lib1.validateDataFrame (snd D.tableInvalid1) `shouldNotBe` Lib1.validateDataFrame (snd D.tableInvalid2)
-    it "passes valid tables" $ do
-      Lib1.validateDataFrame (snd D.tableWithNulls) `shouldBe` Right ()
-  describe "Lib1.renderDataFrameAsTable" $ do
-    it "renders a table" $ do
-      Lib1.renderDataFrameAsTable 100 (snd D.tableEmployees) `shouldSatisfy` not . null
+  describe "Lib2.showtable" $ do
+    it "handles correct input" $ do
+      Lib2.parseStatement "show table flags" `shouldBe` Right (ShowTable "flags")
+    it "handles incorrect names" $ do
+      Lib2.parseStatement "show table asdf" `shouldBe` Left "Invalid show statement: after SHOW keyword no keyword TABLE or TABLES was found"
+    it "handles incorrect number of names" $ do
+      Lib2.parseStatement "show table flags asdf" `shouldBe` Left "Invalid show statement: there can only be one table"
+    it "handles case-insensitive keywords" $ do
+      Lib2.parseStatement "sHoW TaBlE flags" `shouldBe` Right (ShowTable "flags")
+    it "handles case-sensitive names" $ do
+      Lib2.parseStatement "sHoW TaBlE flAgS" `shouldBe` Left "Invalid show statement: no column found with that name"
+  describe "Lib2.showtables" $ do
+    it "handles correct input" $ do
+      Lib2.parseStatement "show tables" `shouldBe` Right ShowTables
+    it "handles incorrect input" $ do
+      Lib2.parseStatement "show tables asdf" `shouldBe` Left ""
+    it "handles case-insensitive keywords" $ do
+      Lib2.parseStatement "shOW tAblEs" `shouldBe` Right ShowTables
+  describe "Lib2.Select" $ do
+    it "handles correct input" $ do
+      Lib2.parseStatement "SELECT flag FROM flags" `shouldBe` Right (Select [ColumnName "flag" Nothing] "flags" [])
+    it "handles correct input with WHERE keyword" $ do
+      Lib2.parseStatement "SELECT flag FROM flags WHERE flag = b" `shouldBe` Right (Select [ColumnName "flag" Nothing] "flags" [Equals (ColumnName "flag" Nothing) "b"])
+    it "handles multiple conditions in correct input" $ do
+      Lib2.parseStatement "SELECT flag, value FROM flags WHERE flag = b AND value = true" `shouldBe` Right (Select [ColumnName "flag" Nothing, ColumnName "value" Nothing] "flags" [Equals (ColumnName "flag" Nothing) "b", Equals (ColumnName "value" Nothing) "true"])
+    it "handles MIN aggregate function" $ do
+      Lib2.parseStatement "SELECT MIN flag FROM flags" `shouldBe` Right (Select [ColumnName "flag" Nothing] "flags" [Min])
+    it "handles AVG aggregate function" $ do
+      Lib2.parseStatement "SELECT AVG id FROM employees" `shouldBe` Right (Select [ColumnName "id" Nothing] "employees" [Avg])
