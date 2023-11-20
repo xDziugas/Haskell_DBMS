a75fe915674ed34dc52a6b680ef0b2afe5f620dd
adaspera <jusciokas1@gmail.com>
2023-11-20 13:44:27 +0200

    merged in 3rd assignement

--- app3/Main.hs
+++ app3/Main.hs
@@ -1,10 +1,14 @@
 module Main (main) where
 
 import Control.Monad.IO.Class (MonadIO (liftIO))
-import Data.Either.Extra (maybeToEither)
+import Control.Monad.Free (Free (..))
+
+import Data.Functor((<&>))
+import Data.Time ( UTCTime, getCurrentTime )
 import Data.List qualified as L
-import InMemoryTables (database)
 import Lib1 qualified
+import Lib2 qualified
+import Lib3 qualified
 import System.Console.Repline
   ( CompleterStyle (Word),
     ExitDecision (Exit),
@@ -22,31 +26,43 @@ final = do
   return Exit
 
 ini :: Repl ()
-ini = do
-  liftIO $ putStrLn "Welcome to select-all-from database! Press [TAB] for auto completion."
+ini = liftIO $ putStrLn "Welcome to select-manipulate database! Press [TAB] for auto completion."
 
 completer :: (Monad m) => WordCompleter m
 completer n = do
-  let names = ["select", "*", "from"]
+  let names = [
+              "select", "*", "from", "show", "table",
+              "tables", "insert", "into", "values",
+              "set", "update", "delete"
+              ]
   return $ Prelude.filter (L.isPrefixOf n) names
 
 -- Evaluation : handle each line user inputs
 cmd :: String -> Repl ()
 cmd c = do
   s <- terminalWidth <$> liftIO size
-  case cmd' s of
+  result <- liftIO $ cmd' s
+  case result of
     Left err -> liftIO $ putStrLn $ "Error: " ++ err
     Right table -> liftIO $ putStrLn table
   where
     terminalWidth :: (Integral n) => Maybe (Window n) -> n
     terminalWidth = maybe 80 width
-    cmd' :: Integer -> Either String String
+    cmd' :: Integer -> IO (Either String String)
     cmd' s = do
-      table <- Lib1.parseSelectAllStatement c
-      df <- maybeToEither ("Table not found: " ++ table) (Lib1.findTableByName database table)
-      _ <- Lib1.validateDataFrame df
-      return $ Lib1.renderDataFrameAsTable s df
+      df <- runExecuteIO $ Lib3.executeSql c 
+      return $ Lib1.renderDataFrameAsTable s <$> df
 
 main :: IO ()
 main =
   evalRepl (const $ pure ">>> ") cmd [] Nothing Nothing (Word completer) ini final
+
+runExecuteIO :: Lib3.Execution r -> IO r
+runExecuteIO (Pure r) = return r
+runExecuteIO (Free step) = do
+    next <- runStep step
+    runExecuteIO next
+    where
+        -- probably you will want to extend the interpreter
+        runStep :: Lib3.ExecutionAlgebra a -> IO a
+        runStep (Lib3.GetTime next) = getCurrentTime >>= return . next
