e21cdf7c98251f2a42993bffcf2ac304d84dd916
VarArnas <144057061+VarArnas@users.noreply.github.com>
2023-11-26 19:20:29 +0200

    tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -35,6 +35,7 @@ main = hspec $ do
                                       [[IntegerValue 1]])
       result <- runExecuteIOTest $ Lib3.executeSql query
       result `shouldBe` expected
+      --join not working
     it "correctly joins tables in the where clause" $ do
       let query = "select name, surname, namee, surnamee from employees, employees2 where id = idd;"
       let expected = Right (DataFrame [Column "name" StringType, Column "surname" StringType, Column "namee" StringType, Column "surnamee" StringType]
@@ -47,17 +48,119 @@ main = hspec $ do
                                       [ [IntegerValue 1, StringValue "Vi", StringValue "Po"], [IntegerValue 2, StringValue "Ed", StringValue "Dl"]])
       result <- runExecuteIOTest $ Lib3.executeSql query
       result `shouldBe` expected
-    it "correctly executes where clause" $ do
-      let query = "select id from employees where id > 1;"
-      let expected = Right (DataFrame [Column "id" IntegerType, Column "name" StringType, Column "surname" StringType]
-                                       [ [IntegerValue 1, StringValue "Vi", StringValue "Po"]])
+    it "correctly finds non-existing tables and stops" $ do
+      let query = "select id from invalid;"
+      let expected = Left "Table not found in InMemoryTables"
       result <- runExecuteIOTest $ Lib3.executeSql query
       result `shouldBe` expected
-    it "correctly doesn't execute invalid tables" $ do
-      let query = "select id from invalid1;"
+    it "correctly executes where LessThan clause" $ do
+      let query = "select * from employees where id < 2;"
       let expected = Right (DataFrame [Column "id" IntegerType, Column "name" StringType, Column "surname" StringType]
-                                       [ [IntegerValue 1, StringValue "Vi", StringValue "Po"]])
-      result <- rnExecuteIOTest $ Lib3.executeSql query
+                                       [[IntegerValue 1, StringValue "Vi", StringValue "Po"]])
+      result <- runExecuteIOTest $ Lib3.executeSql query
       result `shouldBe` expected
-      
+    it "correctly executes where LessEqualThan clause" $ do
+      let query = "select * from employees where id <= 1;"
+      let expected = Right (DataFrame [Column "id" IntegerType, Column "name" StringType, Column "surname" StringType]
+                                      [ [IntegerValue 1, StringValue "Vi", StringValue "Po"]
+                                      ])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "correctly executes where Equal clause" $ do
+      let query = "select * from employees where id = 1;"
+      let expected = Right (DataFrame [Column "id" IntegerType, Column "name" StringType, Column "surname" StringType]
+                                       [[IntegerValue 1, StringValue "Vi", StringValue "Po"]])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "correctly executes where GreaterThan clause" $ do
+      let query = "select * from employees where id > 1;"
+      let expected = Right (DataFrame [Column "id" IntegerType, Column "name" StringType, Column "surname" StringType]
+                                       [ [IntegerValue 2, StringValue "Ed", StringValue "Dl"]])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "correctly executes where GreaterEqualThan clause" $ do
+      let query = "select * from employees where id >= 1;"
+      let expected = Right (DataFrame [Column "id" IntegerType, Column "name" StringType, Column "surname" StringType]
+                                      [ [IntegerValue 1, StringValue "Vi", StringValue "Po"],
+                                        [IntegerValue 2, StringValue "Ed", StringValue "Dl"]
+                                      ])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "correctly points out if the first operator is not the column name and stops" $ do
+      let query = "select * from employees where 2 = id;"
+      let expected = Left "First operand is not a column name: 2"
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "handles boolean values and multiple where clauses conditions with AND" $ do
+      let query = "select * from flags where flag = b and value = False;"
+      let expected = Right (DataFrame [Column "flag" StringType, Column "value" BoolType]
+                                      [[StringValue "b", BoolValue False]])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "handles case-insensitive keywords" $ do
+      let query = "sElEcT * FrOm flags WhERe flag = b anD value = False;"
+      let expected = Right (DataFrame [Column "flag" StringType, Column "value" BoolType]
+                                      [[StringValue "b", BoolValue False]])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "handles null values" $ do
+      let query = "select * from flags;"
+      let expected = Right (DataFrame [Column "flag" StringType, Column "value" BoolType]
+                                      [ [StringValue "a", BoolValue True],
+                                        [StringValue "b", BoolValue True],
+                                        [StringValue "b", NullValue],
+                                        [StringValue "b", BoolValue False]
+                                      ])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "doesn't let multiple columns if aggregate function is present" $ do
+      let query = "select min(id), name from employees;"
+      let expected = Left "Invalid statement"
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "handles multiple whitespaces" $ do
+      let query = "select id,                name   ,     surname      from     employees    where     id    =      1;"
+      let expected = Right (DataFrame [Column "id" IntegerType, Column "name" StringType, Column "surname" StringType]
+                                       [[IntegerValue 1, StringValue "Vi", StringValue "Po"]])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "doesn't let column names be seperated by spaces" $ do
+      let query = "select id name ; surname from employees;"
+      let expected = Left "Invalid statement"
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "doesn't let aggregate function be seperated by spaces" $ do
+      let query = "select min id from employees;"
+      let expected = Left "Invalid statement"
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "returns an empty DataFrame for a valid query with no matching records" $ do
+      let query = "select * from employees where id > 100;"
+      let expected = Right (DataFrame [Column "id" IntegerType, Column "name" StringType, Column "surname" StringType] [])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    it "returns an error for non-existent column in select" $ do
+      let query = "select age from employees;"
+      let expected = Left "One or more columns not found"
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+      --join not working
+    it "correctly handles joins with no matching records" $ do
+      let query = "select name, surnamee from employees, employees2 where id = 999;"
+      let expected = Right (DataFrame [Column "name" StringType, Column "surnamee" StringType] [])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected  
+    it "correctly filters records based on string comparison in where clause" $ do
+      let query = "select * from employees where name = Vi;"
+      let expected = Right (DataFrame [Column "id" IntegerType, Column "name" StringType, Column "surname" StringType] [[IntegerValue 1, StringValue "Vi", StringValue "Po"]])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected 
+    it "handles aggregate functions combined with where clause" $ do
+      let query = "select min(flag) from flags where value = False;"
+      let expected = Right (DataFrame [Column "aggregate" IntegerType] [[StringValue "b"]])
+      result <- runExecuteIOTest $ Lib3.executeSql query
+      result `shouldBe` expected
+    
+    
+    
 
